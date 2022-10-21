fe55aaa459c1bbfda59a41cd83b2906fe634ca8d
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2022-10-21 12:20:31 -0300

    Install flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,12 @@
 module.exports = {
   content: [
     './**/*.{html,js}',
+    'node_modules/flowbite/**/*.js',
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin')
+    require('flowbite/plugin'),
   ],
 }
